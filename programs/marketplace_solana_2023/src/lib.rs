4c845f45bc6fbe0340716286a6d51fc0c42e9f45
SoliditySavvy <knoome@proton.me>
2023-10-18 13:53:57 +0200

    accounts added

--- programs/marketplace_solana_2023/src/lib.rs
+++ programs/marketplace_solana_2023/src/lib.rs
@@ -6,10 +6,33 @@ declare_id!("4pnzxTjn7VxS2fBj3H3ESzxrRhrAdzu79TwQ73LnxgW6");
 pub mod marketplace_solana_2023 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, _name: String) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    admin: Signer<'info>,
+}
+
+#[account]
+pub struct Marketplace {
+    admin: Pubkey,
+    fee: u16,
+}
+
+#[account]
+pub struct Collection {
+    name: String,
+    fee: u16,
+}
+
+#[account]
+pub struct Listing {
+    owner: Pubkey,
+    mint: Pubkey,
+    price: u64,
+    exp: u64,
+}
